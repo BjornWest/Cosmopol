d29736b91aa299db1c2a053f9fcb3f1c37b2ce6d
BjornWest <70112340+BjornWest@users.noreply.github.com>
2022-10-20 12:43:24 +0200

    Update flip.py

--- flip.py
+++ flip.py
@@ -35,6 +35,10 @@ def flip():
     observe = [0,1000,1]
     #graderna på observatörens synfält OBS WIP
     observeDegrees = 45
+    #riktning på observatör
+    observeDirection = [1,0,0]
+    #lutning på kamera, (mäts i radianer) standard = [1,0,0] och utvecklas [cos v,sin v, 0] OBS WIP
+    observeRotation = 0
     #radien på myntet
     r = 1000
     for x in range(frames):
@@ -53,7 +57,7 @@ def flip():
             edge2 = [coinPos[1]+sqrt(r**2-((y-r)**2)),height,coinPos[2]+(r-y)*rotSinPos]
             vector1 = [edge1[0] - observe[0],edge1[1]- observe[1],edge1[2]- observe[2]]
             vector2 = [edge2[0] - observe[0],edge2[1]- observe[1],edge2[2]- observe[2]]
-            proj1X = xRange/2+vector1[0]/vector1[2]*xRange/2
+            proj1X = xRange/2+vector1[0]/vector1[2]*xRange/2    
             proj1Y = yRange/2+vector1[1]/vector1[2]*yRange/2
             proj2X = xRange/2+vector2[0]/vector2[2]*xRange/2
             proj2Y = yRange/2+vector2[1]/vector2[2]*yRange/2
